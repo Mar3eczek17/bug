52ed9dd0cbe84bb2b2343814dfb88eadf43b41d7
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-02-03 10:36:26 +0100

    blog app with registration and possibility to create new entries

--- test.py
+++
@@ -1,5 +0,0 @@
-users = [{"name": "Kamil", "country": "Poland"}, {"name": "John", "country": "USA"}, {"name": "Yeti"}]
-list = [next((sub['name'] for sub in users if sub['country'] == "Poland"), None)]
-print(list)
-print(type(list))
-
